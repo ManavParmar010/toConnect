967cb77fb5f2039227e091593a12a35d22bff071
ManavParmar010 <manavparmar2003@gmail.com>
2025-01-07 09:59:55 +0530

    final commit

--- connection.js
+++ connection.js
@@ -1,14 +1,14 @@
 const mysql = require('mysql');
 
-const connection = function () {   
-const pool = mysql.createPool({
-    connectionLimit: 10,
-    host:  "localhost",
-    user:  "root",
-    password:"Manav@10503",
-    database:"toConnect",
-})
-return pool;
+const connection = function () {
+    const pool = mysql.createPool({
+        connectionLimit: 10,
+        host: "localhost",
+        user: "root",
+        password: "Manav@10503",
+        database: "toConnect",
+    })
+    return pool;
 }
 const dbcon = connection();
-module.exports= dbcon;
+module.exports = dbcon;
